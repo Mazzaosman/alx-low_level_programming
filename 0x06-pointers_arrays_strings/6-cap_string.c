4b174c2b49a74fe3fd0adb7661c1477e2b9878cc
mazzamosman <mazzamos2@gmail.com>
2022-10-27 15:29:43 -0700

    The task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,18 +16,18 @@ char *cap_string(char *str)
 			i++;
 
 		if (str[i - 1] == ' ' ||
-		if str[i - 1] == '\t' ||
-		if str[i - 1] == '\n' ||
-		if str[i - 1] == ',' ||
-		if str[i - 1] == ';' ||
-		if str[i - 1] == '.' ||
-		if str[i - 1] == '!' ||
-		if str[i - 1] == '?' ||
-		if str[i - 1] == '"' ||
-		if str[i - 1] == '(' ||
-		if str[i - 1] == ')' ||
-		if str[i - 1] == '{' ||
-		if str[i - 1] == '}' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
 		i == 0)
 			str[i] -= 32;
 		i++;
