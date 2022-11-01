acc359c37697e8c818c3304604e6e9a439870710
mazzamosman <mazzamos2@gmail.com>
2022-11-01 13:18:12 -0700

    The task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - string character
+ * @s: string given
+ * @c: another character
+ * Return: a strng
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+			return (s);
+	}
+	return ('\0');
+}
